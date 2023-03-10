75b775f581476605ec91d5c06c358ff8423b84c3
isaac castro <isaacokechukwu200021@gmail.com>
2023-03-10 14:10:12 -0800

    fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
     import hidden_4
-    names = [name for name in dir(hidden_4) if not name.startswith('_')]
+    names = dir(hidden_4)
     for name in names:
-        print(name)
+        if name[:2] != "_":
+            print(name)

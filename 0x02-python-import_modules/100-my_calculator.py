e2d34ffaa04764a21de7ab91148ab2f29afeaac7
isaac castro <isaacokechukwu200021@gmail.com>
2023-03-12 16:59:22 -0700

    fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """ a prigarm that make operation on numbers"""
-    from calculator_load_5 import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div
     import sys
     if len(sys.argv) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")

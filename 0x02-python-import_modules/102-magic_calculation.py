17b9fc00acae513f96524b231a596f0a4755c456
isaac castro <isaacokechukwu200021@gmail.com>
2023-03-12 17:58:50 -0700

    done

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,9 +2,10 @@
 """match bytecode provided by holberthon school"""
 from magic_calculation_102 import add, sub
 if a < b:
-    c = add(a, b)
-    for i in range(4, 6):
-        c = add(c, i)
-        return(c)
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+
     else:
         return(sub(a, b))

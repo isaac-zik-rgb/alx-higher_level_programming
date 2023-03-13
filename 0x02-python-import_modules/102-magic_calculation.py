3b9992602b55884844b6c91bc1abdbd08067a110
isaac castro <isaacokechukwu200021@gmail.com>
2023-03-12 18:04:06 -0700

    set

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,8 +4,7 @@ from magic_calculation_102 import add, sub
 if a < b:
         c = add(a, b)
         for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-
-    else:
-        return(sub(a, b))
+                c = add(c, i)
+                return (c)
+        else:
+                return (sub(a, b))

53583965bf4f2ee1a95c15559a89b57787455ac0
isaac castro <isaacokechukwu200021@gmail.com>
2023-03-14 16:24:24 -0700

    done

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,5 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return(my_list)
-    for i in list(my_list):
-        if i == idx:
-            my_list[indx] = element
-            return(my_list)
+    my_list[index] = element
+    return(my_list)

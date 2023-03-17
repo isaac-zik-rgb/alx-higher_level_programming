3cb9832de6d63cb808171c380856d9f7ed760554
isaac castro <isaacokechukwu200021@gmail.com>
2023-03-17 15:15:34 -0700

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,4 @@
 def best_score(a_dictionary):
     if is not a_dictionary:
         return None
-    a_dictionary.sort(keys)
-    return(a_dictionary[-1])
+    return (max(a_dictionary, key=a_dictionary.get))

7c518116e746c37ca08394f835c9f4b559ef8e45
isaac castro <isaacokechukwu200021@gmail.com>
2023-03-16 17:28:49 -0700

    fixed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    new_list = my_list.replace(search, replace)
+    for i in range(len(my_list)):
+        new_list = my_list.replace(search, replace)
     return(new_list)

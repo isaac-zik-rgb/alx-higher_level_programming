05421dc8f425264f6d68bfa93b9a6aae4371f1bd
isaac castro <isaacokechukwu200021@gmail.com>
2023-03-16 18:37:25 -0700

    done

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """replace or add key/value pairs in dictionaries"""
+    a_dictionary[key] = value
+    return(a_dictionary)

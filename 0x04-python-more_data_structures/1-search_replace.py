8a356ceb40df7bcfb32f21f7263def985b56591b
isaac castro <isaacokechukwu200021@gmail.com>
2023-03-16 17:37:42 -0700

    set

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     for i in range(len(my_list)):
-        new_list = my_list.replace(search, replace)
+        if i == search:
+            new_list = my_list.replace(search, replace)
     return(new_list)

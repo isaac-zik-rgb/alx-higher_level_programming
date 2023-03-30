2ede9599f3f7c6ad7fd48e4de3de5e92fe15af61
Isaac castro <isaacokechukwu200021@gmail.com>
2023-03-29 18:28:28 -0700

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a class square"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """initiate a new Square.
+Args:
+size (init): size of the new square"""
+        self._size = size

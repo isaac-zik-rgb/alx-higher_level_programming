c518bc457873785facc3b8083e6e612166e96a4a
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-04-11 23:34:26 +0000

    done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define a class BaseGeometry"""
+
+
+def BaseGeometry:
+    """an empty class"""
+
+    def area(self):
+        """not implemented"""
+        raise Exception("area() is not implemented")

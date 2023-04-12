7aa865b972d95114075e974c89f329f8b4fdf1e7
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-04-12 01:08:28 +0000

    all set

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
+
 """Rectangle subclass Square."""
-
-
 Rectangle = __import__('9-rectangle').Rectangle
+
+
 class Square(Rectangle):
     """Represent a square."""
     def __init__(self, size):

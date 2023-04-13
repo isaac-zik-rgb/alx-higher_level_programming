7426edfa5d4d8df5218cfc804f3cf112fb248ea6
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-04-13 23:56:50 +0000

    test2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/python3
+
+"""write a function that append a texts at the end of a file using
+UTF-8 encoding format"""
+
+
+def append_write(filename="", text=""):
+    """Args:
+append_write: append a text at the end of a file
+filename (obj): the name if the file
+text (str): the string to be appended
+Returns:
+number of characters to be appended"""
+    with open(filename, "a", enconding="utf-8") as f:
+        return (f.write(text))

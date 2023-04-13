8cd2c4b77588cf50919d5e65b6372083b9ee9791
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-04-13 23:35:54 +0000

    test

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """prints the contents of a file in a UTF-8 format"""
-    with open("filename", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")

1cd209a6fff31e9a1321273897d2d8c0bcf3a0a2
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-04-14 02:35:05 +0000

    set

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Define a class Student"""
+
+
+class Student:
+    """A class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Args:
+first_name (str) : the first name of the student
+last_name (str): the last name of the student
+age (int) : the age of the student
+Returns:
+the dictionary representation of the Student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        """A public method tgat retrives the dictionary representation of the student"""
+        return self.__dict__

68290b519079ddd6772015e56dd1346dc32a66c0
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-05-17 22:36:34 +0000

    set

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,4 @@
 -- script that display number of records with id = 89 in first_table in mysql server
-SELECT * FROM `first_table` WHERE `id` = '89';
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = '89';

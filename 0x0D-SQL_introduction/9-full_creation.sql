6bca83d63801bc0344e6384e1dbf3a4d0398fda8
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-05-17 22:55:31 +0000

    done

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- script that create a second table with multiply rows in mysql server
+CREATE TABLE IF NOT EXISTS `second_table`(`id`, `name`, `score`) VALUES(INT, VARCHAR(256), INT);
+INSERT INTO `second_table`(`id`, `name`, `score`) VALUES('1', 'John', '10');
+INSERT INTO `second_table`(`id`, `name`, `score`) VALUES('2', 'Alex', '3');
+INSERT INTO `second_table`(`id`, `name`, `score`) VALUES('3', 'Bob', '14');
+INSERT INTO `second_table`(`id`, `name`, `score`) VALUES('4', 'George', '8');

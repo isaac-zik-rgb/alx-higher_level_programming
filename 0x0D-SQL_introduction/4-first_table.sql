0888e68f4dbb584d5032f59b0fe1c7ef1f3da492
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-05-17 21:55:04 +0000

    set

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,7 @@
--- a script that create new table in the current database wirh
--- description
-CREATE TABLE IF NOT EXISTS first_table (
-id INT,
-name VARCHAR(256),
+-- a script that create new table in the current database with
+-- id and name
+CREATE TABLE IF NOT EXISTS `first_table` (
+`id` INT,
+`name` VARCHAR(256),
 );
+

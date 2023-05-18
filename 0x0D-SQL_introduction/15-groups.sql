3ab1940a252ccf2d618fd9e22a55cf1824c68fb7
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-05-18 09:09:14 +0000

    set

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,5 @@
+-- list number of record with the same score in the second_table in my sql server
+-- score wipl be order in descending order
 SELECT `score`, COUNT(*) AS `number`
 FROM `second_table`
 GROUP BY `score`

99aee55556b796faff19550946c73bb04e6a7785
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-05-18 10:43:16 +0000

    selt

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,5 @@
 -- Dispjay the average temperature of all city in hbtn_0c_0 database in mysql server
 SELECT `city`, AVG(`value`) AS avg_temp;
 FROM `Temperatures`
+GROUP BY `city`
 ORDER BY `avg_temp` DESC;

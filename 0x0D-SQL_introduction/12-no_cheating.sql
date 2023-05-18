dd2e24ba05c2e50621546e82f150d17fc81241df
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-05-18 08:27:13 +0000

    set

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,5 @@
 -- update the name Bob with the score 10
 -- list ib descending order in mysql sever
 UPDATE `second_school`
-SET `score` = '10'
-WHERE `name` = 'Bob`;
+SET `score` = 10
+WHERE `name` = 'Bob';

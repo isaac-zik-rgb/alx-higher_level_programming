e3efa64d40f6d94c19b44d825fc2b19a042f003a
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-05-18 15:49:45 +0000

    set

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,5 @@
 -- list the max value of cities that has the same temperature
-SELECT `city`, MAX(`value`) AS max_temp
-FROM `Temperatures`
+SELECT `city`, MAX(`value`) AS `max_temp`
+FROM `temperatures`
 GROUP BY `state`
 ORDER BY `state`;
-

24cf4e1794cf007fb9e17a4a5549e20ab9ae5c88
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-05-20 11:35:41 +0000

    set

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,4 +7,4 @@ USE hbtn_0d_usa;
 -- create table
 CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE NOT NULL AUTO_INCREMENT,
 state_id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY(id),
-FOREIGN KEY(state_id), REFERENCES states(id));
+FOREIGN KEY(state_id) REFERENCES states(id));

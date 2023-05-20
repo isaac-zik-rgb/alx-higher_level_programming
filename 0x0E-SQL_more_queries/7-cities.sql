0333bca9fc2d43b4225fe098550fa6f1a57cd2b7
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-05-20 11:34:08 +0000

    done

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- create  database hbtn_0d_usa and table cites (in the database hbtn_0d_usa)
+-- in mysql server.
+-- create database
+CREATE DATABASE IF NIT EXISTS hbtn_0d_usa;
+-- use database
+USE hbtn_0d_usa;
+-- create table
+CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE NOT NULL AUTO_INCREMENT,
+state_id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY(id),
+FOREIGN KEY(state_id), REFERENCES states(id));

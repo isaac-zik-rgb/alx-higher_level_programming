795fb2058956d76aacf08c4664220c4a6eb8f372
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-05-20 09:24:36 +0000

    set

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- create a table id_not_null on mysql server
 --description: id INT, name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS `id_not_null`(`id` INT(1), `name` VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `id_not_null`(`id` INT DEFAULT 1, `name` VARCHAR(256));

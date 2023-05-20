f05712b4932c6f939030744e226dc51620b95e4b
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-05-20 12:58:33 +0000

    set

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- lists all the cities of California that can not found in the database hbtn_0d_usa
 -- lists all rows of a column in a database
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = ca'Calofornia') ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'Calofornia') ORDER BY id ASC;

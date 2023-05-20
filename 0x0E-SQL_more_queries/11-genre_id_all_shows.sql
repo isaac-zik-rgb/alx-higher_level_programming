bf275030a7a041acce160ae10c77400662bea189
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-05-20 18:09:55 +0000

    set

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
--- A script that list all shows contained in the database hbtn_0d_tvshows
--- list all shows that has genre linked, if no show has genre display NULL
+-- lists all shows contained in the database hbtn_0d_tvshows
+-- lists all rows of tables in a database
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN
 tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY
 tv_show.title ASC; tv_show_genres.genre_id ASC;

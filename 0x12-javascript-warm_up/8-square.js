7cc9f3c05f8ff9ca012baf8ff82923039faa3eb7
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-06-14 02:18:55 -0700

    task8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ if (Number(process.argv[2])) {
   for (let count = 0; count < Number(process.argv[2]); count++) {
     square = '';
     for (let i = 0; i < Number(process.argv[2]); i++) {
-      square += 'x';
+      square += 'X';
     }
     console.log(square);
   }

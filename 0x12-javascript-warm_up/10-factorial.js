7877fd8eaf306d442fb2ebd26791eddadce2219d
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-06-14 16:43:48 -0700

    done

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
-function factorial(n){
-    if (n){
-	if(n === 0){
-	    return 1;
-	}
-	else if (n > 89){
-	    return('Infinity');
-	}
-	return n * factorial(n - 1);
-    }else
-	return 1;
+function factorial (n) {
+  if (n) {
+    if (n === 0) {
+      return 1;
+    } else if (n > 89) {
+      return ('Infinity');
+    }
+    return n * factorial(n - 1);
+  } else { return 1; }
 }
-
 
 console.log(factorial(Number(process.argv[2])));
--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const add = (a, b) => {
-    return a + b;
-}
+  return a + b;
+};

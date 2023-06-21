e6938ab1344f32fb7abbe7f0751ba2136f08cac4
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-06-21 08:07:37 -0700

    task8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list){
     let reverseList = [];
-    for (let i = list.length; i >= 0; i--){
+    for (let i = list.length - 1; i >= 0; i--){
 	reverseList.push(list[i]);
     }
     return reverseList;

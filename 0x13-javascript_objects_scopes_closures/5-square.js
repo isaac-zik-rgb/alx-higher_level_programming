68cb3b0c9f42d950a4e5cb409965aee4f5fb00ac
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-06-21 13:02:24 +0000

    task5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-
+const Rectangle = require(`./4-rectangle`);
 class Square extends Rectangle {
     constructor(size){
-	super(w, h);
+	super(size, size);
     }
 }
 module.exports = Square;

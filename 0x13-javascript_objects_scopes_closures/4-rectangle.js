db19616a94a307c7f84e5edf978a1e258a77054d
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-06-21 12:49:49 +0000

    task4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor(w, h){
+	this.width = w;
+	this.height = h;
+    }
+    print (){
+	for (let i = 0; i < this.height; i++){
+	    let x = '';
+	    for (let j = 0; j < this.width; j++){
+		x += "X";
+	    }
+	    console.log(x);
+	}
+    }
+    rotate (){
+	const swap = this.width;
+	this.width = this.height;
+	this.height = swap;
+    }
+    double (){
+	this.width *= 2;
+	this.height *= 2;
+    }
+}
+module.exports = Rectangle;

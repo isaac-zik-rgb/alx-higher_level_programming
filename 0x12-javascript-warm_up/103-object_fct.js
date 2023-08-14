7167a99b04e99b62356ab87ea1f3c05ef4575c63
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-08-14 20:18:06 +0100

    done

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
+  type: 'object',
+  value: 12
 };
 console.log(myObject);
-myObject.incr = function incr(){
-    myObject.value++;
-}
+
+myObject.incr = function () {
+  this.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();

3bfe2c1ed78b9abf1acab9d636f57d0a4df6e352
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-08-14 20:39:26 +0100

    done

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
+  type: 'object',
+  value: 12
 };
 console.log(myObject);
-myObject.incr = function () {
-    this.value++;
+
+// Add the incr function to myObject using property shorthand
+myObject.incr = function incr () {
+  this.value++;
 };
 
 myObject.incr();

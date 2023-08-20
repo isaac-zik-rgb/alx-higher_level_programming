9b0685dce5e74c5e963beb43b8325ab38a1cb1be
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-08-20 22:27:41 +0100

    task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,45 +2,18 @@
 import MySQLdb
 import sys
 """ import all method of the class MySQLdb"""
+if __name__ == "__main__":
 
-def list_states(username, password, database):
-     """
-    Retrieve and display a list of states from the given MySQL database.
-
-    Args:
-        username (str): MySQL username for database access.
-        password (str): MySQL password for database access.
-        database (str): Name of the database to retrieve states from.
-
-    Returns:
-        None
-
-    Function-level attributes:
-        - None
-    """
-     
-     db = MySQLdb.connect(host='localhost', port=3006, user=username, passwd=password, db=database)
+     db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
      cursor = db.cursor()
-     """Execute SQL query to retrieve states"""
-     query = "SELECT * FROM states ORDER BY states.id ASC"
-     cursor.execute(query)
-     """fetch all rows"""
+     cursor.execute("SELECT * FROM states")
+     """fetch the data"""
      rows = cursor.fetchall()
-
+     """print the rows"""
      for row in rows:
           print(row)
 
-          """close connection"""
+     """close connection for the cursor object and database"""
      cursor.close()
      db.close()
-          
-if __name__ == "__main__":
-
-    if(len(sys.argv) != 4):
-        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
-    else:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-        list_states(username, password, database)
-        
+     

3c93c3f5a8b323ae0dbc8326de5e614ab271eb69
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-08-20 22:36:56 +0100

    task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
-""" import all method of the class MySQLdb"""
+
 if __name__ == "__main__":
 
      db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)

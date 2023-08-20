ae19b1e5f98009986e803002bc8f563c9fffb407
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-08-20 22:15:04 +0100

    task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,29 +4,43 @@ import sys
 """ import all method of the class MySQLdb"""
 
 def list_states(username, password, database):
-    """List all states of the user in mysql"""
-    db = MySQLdb.connect(host='localhost', port=3006, user=username, passwd=password, db=database)
-    cursor = db.cursor()
-    """Execute SQL query to retrieve states"""
-    query = "SELECT * FROM states ORDER BY states.id ASC"
-    cursor.execute(query)
-    """fetch all rows"""
-    rows = cursor.fetchall()
+     """
+    Retrieve and display a list of states from the given MySQL database.
 
-    for row in rows:
-        print(row)
+    Args:
+        username (str): MySQL username for database access.
+        password (str): MySQL password for database access.
+        database (str): Name of the database to retrieve states from.
 
-    """close connection"""
-    cursor.close()
-    db.close()
+    Returns:
+        None
 
+    Function-level attributes:
+        - None
+    """
+     
+     db = MySQLdb.connect(host='localhost', port=3006, user=username, passwd=password, db=database)
+     cursor = db.cursor()
+     """Execute SQL query to retrieve states"""
+     query = "SELECT * FROM states ORDER BY states.id ASC"
+     cursor.execute(query)
+     """fetch all rows"""
+     rows = cursor.fetchall()
+
+     for row in rows:
+          print(row)
+
+          """close connection"""
+     cursor.close()
+     db.close()
+          
 if __name__ == "__main__":
 
     if(len(sys.argv) != 4):
         print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
     else:
-        username = argv[1]
-        password = argv[2]
-        database = argv[3]
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
         list_states(username, password, database)
         

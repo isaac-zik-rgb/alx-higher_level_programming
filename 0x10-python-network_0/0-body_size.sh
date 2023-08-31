04c16d34129adc42403cd381bae7ce9cce796f0e
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-08-31 20:58:18 +0000

    task2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL, sends a request, and displays the response body size in bytes
-curl -sI "$1" | grep -i "content-length" | awk '{print $2}' | tr -d '\r'
+curl -sI "$1" | wc -c
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+# A script that sends a DELETE request to a URI passed to it
+curl -sX DELETE "$1"

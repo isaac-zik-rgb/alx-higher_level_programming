bf540975b1e97cabcbdb5d956b35f2fac42495f0
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-08-31 17:34:22 +0000

    task1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that uses `curl` command to send a GET request and display a 200 respond message
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ "$response" -eq 200 ] && curl -s "$1"
+curl -sL "$1"

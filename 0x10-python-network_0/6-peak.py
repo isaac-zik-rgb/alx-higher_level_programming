bfb8f8ef023ba2888f94e5657382fff59634704a
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-01 14:21:45 +0100

    done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,6 +6,21 @@ find_peak - A function that find the peak of list of numbers passed to it
 Args:
 @list_of_intergers: the list of numbers passed to it
 """
-    list_of_integers.sort(reverse=True)
-    firstNum = list_of_integers[0]
-    return firstNum
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    first = 0
+    last = len(list_of_integers)
+    mid = ((last + first) // 2) + first
+    mid = int(mid)
+    if last == 1:
+        return list_of_integers[0]
+    if last == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+       list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))

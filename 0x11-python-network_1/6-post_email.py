aeb48f562846df7cef83619128547195f2c3ef4b
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-01 22:41:00 +0100

    task6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a paramete"""
+if __name__ == "__main__":
+    import sys
+    import requests
+    url = sys.argv[1]
+    res = requests.get(url)
+    res.post(url, data={'email': sys.argv[2]})
+    print("Your email is: {}".format(data['email']))

b34594bbe21f7fa4ea1d1686c88106b7c2006340
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-01 22:06:30 +0100

    task4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Python script that fetches https://alx-intranet.hbtn.io/status"""
 if __name__ == "__main__":
-     import requests
-     res = requests.get('https://alx-intranet.hbtn.io/status')
-     print("Body response:")
-     print("\t- type: <class 'str'>")
-     print("\t- content: {}".format(res.text))
+    import requests
+
+    res = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: <class 'str'>")
+    print("\t- content: {}".format(res.text))

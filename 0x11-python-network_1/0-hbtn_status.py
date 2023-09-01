d70444c8bbc8d2e583309bcadc30b4ccd49584b5
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-01 18:39:31 +0100

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,6 @@
 
 if __name__ == "__main__":
     import urllib.request
-    
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
          as response:
         content_string = response.read()

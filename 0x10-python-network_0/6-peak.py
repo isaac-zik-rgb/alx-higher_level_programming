751c5ee2cb387efcfc575273a176f82c6ad83326
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-01 02:56:03 +0000

    set

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+    """
+find_peak - A function that find the peak of list of numbers passed to it
+Args:
+@list_of_intergers: the list of numbers passed to it
+"""
+    list_of_integers.sort(reverse=True)
+    firstNum = list_of_integers[1]
+    for i in list_of_integers:
+        if i == firstNum:
+            return i, firstNum
+        else:
+            return firstNum

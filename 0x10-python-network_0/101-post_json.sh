7040766fb68b450e026f0ea5330aae38098926c6
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-01 15:05:51 +0100

    task101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request to a given URI with a given JSON file
-curl -s -H "Content-Type:application/json" -d "$(cat "$2") "$1"
+curl -s -H "Content-Type:application/json" -d "$(cat "$2")" "$1"

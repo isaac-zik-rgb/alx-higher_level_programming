d8291b10b53d4293c874f70721bf779fe5c72699
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-01 18:32:52 +0100

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,11 @@
 if __name__ == "__main__":
     import urllib.request
     """import request from urllib libary"""
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
+         as response:
         content_string = response.read()
         content_byte = content_string.decode('utf-8')
         print("Body response:")
         print("\t - type: {}".format(type(content_string)))
         print("\t - content: {}".format(content_string))
         print("\t - utf8 content: {}".format(content_byte))
-        

98c3aa4bd324cca6e1e671ee662e0a0c60fdcafb
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-01 18:38:09 +0100

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-""" A script that fetches a uri and disply its body"""
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
+
 if __name__ == "__main__":
     import urllib.request
-    """import request from urllib libary"""
+    
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
          as response:
         content_string = response.read()
         content_byte = content_string.decode('utf-8')
         print("Body response:")
-        print("\t - type: {}".format(type(content_string)))
-        print("\t - content: {}".format(content_string))
-        print("\t - utf8 content: {}".format(content_byte))
+        print("\t- type: {}".format(type(content_string)))
+        print("\t- content: {}".format(content_string))
+        print("\t- utf8 content: {}".format(content_byte))

b3d978332eb46f0c884fda61017faf96a89ec48b
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-01 20:21:26 +0100

    task2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Script that takes in a URL and an email, sends a POST request
+to the passed URL with the email and display the response"""
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode(email)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
+        print("Your email is: {}".format(email))
+        

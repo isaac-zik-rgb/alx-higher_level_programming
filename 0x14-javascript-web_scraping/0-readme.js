78c9823a19054aa13bef0f222e3c369a48765095
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-26 16:05:55 +0100

    task0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/nodee
+const { readFile } = require('fs');
+const filePath = process.argv[2];
+
+readFile(filePath, 'utf-8', (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
+});

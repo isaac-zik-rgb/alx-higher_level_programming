3b5493b0f9fef12e488025e88c9a4a35249cadfc
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-26 17:59:11 +0100

    wedgeAntellesMovies

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
+
 const apiUrl = process.argv[2];
 const characterId = 18;
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
-    return;
-  }
-  if (response.statusCode !== 200) {
-    console.log('Error:', `Received status code ${response.statusCode}`);
     return;
   }
   const filmsData = JSON.parse(body).results;

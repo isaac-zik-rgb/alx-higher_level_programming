32a928d20090d335db79905cef2564c7abe10c5a
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-10-04 21:21:27 +0100

    task1 - task5

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+$(function () {
+  $('#toggle_header').on('click', function () {
+    if ($('header').hasClass('red')) {
+      $('header').removeClass('red').addClass('green');
+    } else {
+      $('header').removeClass('green').addClass('red');
+    }
+  });
+});
